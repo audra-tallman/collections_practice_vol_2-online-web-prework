adf49986afe896d17aaab74e65298a4297194e1f
Audra Tallman <audra.tallman@hotmail.com>
2019-03-04 19:34:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -50,20 +50,3 @@ def count_elements(array)
   end
   .uniq
 end
-  
-  array.each_with_object(Hash.new(0)) { |array,counts| counts[array] += 1 }
-end
-
-
-
-def count_elements(array)
-  array.each do |original_hash|
-    original_hash[:count] = 0
-    name = original_hash[:name]
-    array.each do |hash|
-      if hash[:name] == name
-        original_hash[:count] += 1
-      end
-    end
-  end.uniq
-end
